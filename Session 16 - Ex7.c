f4bfd90a98d49eedbba7fe74aff50dcffb9c1ff0
tientda1 <110662513+tientda1@users.noreply.github.com>
2024-12-16 15:03:32 +0700

    Update Session 16 - Ex7.c

--- Session 16 - Ex7.c
+++ Session 16 - Ex7.c
@@ -4,7 +4,6 @@ void sortArray(int *arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Hoán d?i hai ph?n t?
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
